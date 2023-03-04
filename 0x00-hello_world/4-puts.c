9522c53a6e11ebe64040cbeb7cd03350d8340aab
noahwekesa <noahwekesa0@gmail.com>
2023-03-04 01:43:04 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints "Programing is like building a multiligual puzzle.
+ * Return: Always 0.
+ * /
+
+
+ int main(void)
+ {
+    put("\"programing is like building a multiligual puzzle");
+    return (0);
+  }
