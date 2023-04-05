4e4adce7531de1a9051c7398ba126e1433ff467f
noahwekesa <noahwekesa0@gmail.com>
2023-04-06 00:18:53 +0300

    fist task

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * file: 0-puts_recursion.c - prints a string followed by new line  
+ * @x:string
+ * Return: always 0
+ * 
+ */
+
+void _puts_recursion(char *x)
+
+{
+    if(*x)
+    {
+        _putchar(*x);
+        _puts_recursion(x+1);
+    }
+    else
+    {
+        _putchar("\n");
+    }
+}
