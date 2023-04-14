cf43aab01c2994cb72b9978e522d9707844a4df9
noah wekesa <noahwekesa0@gmail.com>
2023-04-14 01:22:41 -0700

    error

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * *malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
+ *
+ * Return: pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+	p = malloc(b);
+	if(p == Null)
+		exit(98);
+	return(p);
+}
