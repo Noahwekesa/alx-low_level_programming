dffe78aa67bb4acd67e4a4a0f2b60611069b17d1
noahwekesa <noahwekesa0@gmail.com>
2023-05-05 16:34:29 -0700

    wayen

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -2,7 +2,7 @@
 
 /**
  * clear_bit - program that sets the value of
- *  a bit to 0 at a given index.
+ * a bit to 0 at a given index.
  * @n: pointer to the number to change
  * @index: index of the bit to clear
  *
