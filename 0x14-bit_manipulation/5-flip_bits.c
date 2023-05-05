2d720518660fdcf16f5325ebe01406b68fb16d8b
noahwekesa <noahwekesa0@gmail.com>
2023-05-05 16:28:06 -0700

    trailing spaces

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,23 +1,23 @@
 #include "main.h"
 
 /**
- * flip_bits - program that returns the number of bits you 
+ * flip_bits - program that returns the number of bits you
  * would need to flip to get from one number to another.
  * @n: the first value
  * @m: the second value
+ *
+ * Return: output
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-    unsigned long int cur_bits = n ^ m;
-    unsigned int numbits = 0;
+	unsigned long int cur_bits = n ^ m;
+	unsigned int numbits = 0;
 
-    while (cur_bits > 0)
-    {
-        if (cur_bits & 1)
-        numbits++;
-
-        cur_bits >>= 1;
-    }
-
-    return (numbits);
+	while (cur_bits > 0)
+	{
+		if (cur_bits & 1)
+			numbits++;
+		cur_bits >>= 1;
+	}
+	return (numbits);
 }
