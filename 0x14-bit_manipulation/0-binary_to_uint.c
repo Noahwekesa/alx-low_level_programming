6995d6ddec6b8903617efa8d3e0b491aaed0bdbf
noahwekesa <noahwekesa0@gmail.com>
2023-05-26 02:15:14 -0700

    h

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,7 +2,8 @@
 #include <stddef.h>
 
 /**
- * binary_to_uint – program that converts a binary number to an unsigned int.
+ * binary_to_uint –program that converts a binary number to an unsigned int.
+ *
  * @b: is pointing to a string of 0 and 1 chars
  *
  * Return: the converted number, or 0 if there is one
