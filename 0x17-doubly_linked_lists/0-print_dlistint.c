ad187a9687882c3fad4f33e549450bdbf019b677
noahwksa <noahwekesa0gmail.com>
2023-08-17 22:43:09 +0300

    shutdown

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - a program that prints all the elements of
+ * a double linked list
+ * @h
+ * 
+ * Return: The number of the nodes of the double linked list
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+    int lenght = 0;
+
+    while (h != NULL)
+    {
+        printf("%d\n", h->);
+        ++lenght;
+        h = h->next;
+    }
+
+    return (lenght);
+}
